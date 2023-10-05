d9b3dd19b9045ad1c69e4624a48ea096b6bfc0df
Siddharth U <138808939+siddharthyou29@users.noreply.github.com>
2023-10-05 10:45:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor('black')
+screen.bgpic('background.gif')
+screen.setup(width=800, height=600)
+screen.title('Pong')
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+game_is_on = True
+
+screen.listen()
+screen.onkeypress(r_paddle.go_up, 'Up')
+screen.onkeypress(r_paddle.go_down, 'Down')
+screen.onkeypress(l_paddle.go_up, 'w')
+screen.onkeypress(l_paddle.go_down, 's')
+screen.onkey(screen.bye, 'x')
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        while ball.distance(r_paddle) < 50 or ball.distance(l_paddle) < 50:
+            ball.move()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+screen.exitonclick()
